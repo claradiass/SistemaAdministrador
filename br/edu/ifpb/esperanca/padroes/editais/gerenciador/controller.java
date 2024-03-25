affba06f3bcc3b3bae20a205ec0c26a27686c8a7
claradiass <claragoncalves370@gmail.com>
2024-03-24 21:33:17 -0300

    tentativa de efetivar a função de criar aluno

--- br/edu/ifpb/esperanca/padroes/editais/gerenciador/controller.java
+++ br/edu/ifpb/esperanca/padroes/editais/gerenciador/controller.java
@@ -3,30 +3,72 @@ package br.edu.ifpb.esperanca.padroes.editais.gerenciador;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.edu.ifpb.esperanca.padroes.editais.facade.FacadeAdministrator;
+import br.edu.ifpb.esperanca.padroes.editais.model.Name;
+import br.edu.ifpb.esperanca.padroes.editais.model.Password;
 import br.edu.ifpb.esperanca.padroes.editais.model.Professor;
 import br.edu.ifpb.esperanca.padroes.editais.model.Student;
+import br.edu.ifpb.esperanca.padroes.editais.model.UserName;
 
 
 
-public class controller {
-    private List<Student> students = new ArrayList<>();
-    private List<Professor> professores = new ArrayList<>();
+public class controller implements FacadeAdministrator{
 
-    public controller(List<Student> students, List<Professor> professores) {
-        this.students = students;
-        this.professores = professores;
+    @Override
+    public void createStudentAccount(String usuario, String nome, String senha) {
+        Name name = new Name(nome);
+        Password password = new Password(senha);
+        UserName userName = new UserName(usuario);
+
+        Student student = new Student(name, password, userName);
+
+        
     }
 
-    public void addStudent(List<Student> students, Student student){
-        students.add(student);
-    }   
 
-    public void addProfessor(List<Professor> professores, Professor professor){
-        professores.add(professor);
+
+
+    @Override
+    public void createProfessorAccount(String userName, String nome, String senha) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createProfessorAccount'");
     }
 
-    public void removeStudent(List<Student> students, Student student){
-        students.remove(student);
+    @Override
+    public void deleteStudentAccount(int idStudent) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteStudentAccount'");
     }
+
+    @Override
+    public void deleteProfessorAccount(int idProfessor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteProfessorAccount'");
+    }
+
+    @Override
+    public List<Student> viewStudentList() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'viewStudentList'");
+    }
+
+    @Override
+    public List<Professor> viewProfessorList() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'viewProfessorList'");
+    }
+
+    @Override
+    public void updateStudentAccoun(int idStudent) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateStudentAccoun'");
+    }
+
+    @Override
+    public void updateProfessorAccoun(int idProfessor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateProfessorAccoun'");
+    }
+    
 
 }
